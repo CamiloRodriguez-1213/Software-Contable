182257bfa79896e4920f670fd297faee72332355
Camilo Rodriguez <andrescamilocr482@gmail.com>
2022-05-16 15:31:21 -0500

    Database

--- db.sql
+++ db.sql
@@ -13,51 +13,37 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Volcando estructura de base de datos para software_contable
-CREATE DATABASE IF NOT EXISTS `software_contable` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `software_contable`;
+-- Volcando estructura de base de datos para software_contables
+CREATE DATABASE IF NOT EXISTS `software_contables` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `software_contables`;
 
--- Volcando estructura para tabla software_contable.cargos
+-- Volcando estructura para tabla software_contables.cargos
 CREATE TABLE IF NOT EXISTS `cargos` (
   `id_cargo` int(11) NOT NULL AUTO_INCREMENT,
-  `descripcion` varchar(100) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id_cargo`) USING BTREE
+  `descripcion` varchar(50) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_cargo`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla software_contable.cargos: ~1 rows (aproximadamente)
+-- Volcando datos para la tabla software_contables.cargos: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `cargos` DISABLE KEYS */;
 INSERT INTO `cargos` (`id_cargo`, `descripcion`) VALUES
 	(1, 'administrador');
 /*!40000 ALTER TABLE `cargos` ENABLE KEYS */;
 
--- Volcando estructura para tabla software_contable.categorias
+-- Volcando estructura para tabla software_contables.categorias
 CREATE TABLE IF NOT EXISTS `categorias` (
   `id_categoria` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_categoria`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla software_contable.categorias: ~1 rows (aproximadamente)
+-- Volcando datos para la tabla software_contables.categorias: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
 INSERT INTO `categorias` (`id_categoria`, `nombre`) VALUES
-	(1, 'Lacteos');
+	(1, 'Bebidas');
 /*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
 
--- Volcando estructura para tabla software_contable.clientes
-CREATE TABLE IF NOT EXISTS `clientes` (
-  `id_cliente` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(50) DEFAULT NULL,
-  `apellido` varchar(50) DEFAULT NULL,
-  `celular` varchar(50) DEFAULT NULL,
-  `correo` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id_cliente`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Volcando datos para la tabla software_contable.clientes: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
-/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
-
--- Volcando estructura para tabla software_contable.empleados
+-- Volcando estructura para tabla software_contables.empleados
 CREATE TABLE IF NOT EXISTS `empleados` (
   `id_empleado` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) NOT NULL DEFAULT '',
@@ -68,151 +54,95 @@ CREATE TABLE IF NOT EXISTS `empleados` (
   `direccion` varchar(50) DEFAULT NULL,
   `id_login` int(11) DEFAULT NULL,
   `id_cargo` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_empleado`) USING BTREE,
-  KEY `FK_id_login` (`id_login`),
-  KEY `FK_id_cargo` (`id_cargo`),
-  CONSTRAINT `FK_id_cargo` FOREIGN KEY (`id_cargo`) REFERENCES `cargos` (`id_cargo`),
-  CONSTRAINT `FK_id_login` FOREIGN KEY (`id_login`) REFERENCES `login` (`id_login`)
+  PRIMARY KEY (`id_empleado`),
+  KEY `FK_empleados_login` (`id_login`),
+  KEY `FK_empleados_cargos` (`id_cargo`),
+  CONSTRAINT `FK_empleados_cargos` FOREIGN KEY (`id_cargo`) REFERENCES `cargos` (`id_cargo`),
+  CONSTRAINT `FK_empleados_login` FOREIGN KEY (`id_login`) REFERENCES `login` (`id_login`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla software_contable.empleados: ~0 rows (aproximadamente)
+-- Volcando datos para la tabla software_contables.empleados: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `empleados` DISABLE KEYS */;
 INSERT INTO `empleados` (`id_empleado`, `nombre`, `apellido`, `identificacion`, `celular`, `correo`, `direccion`, `id_login`, `id_cargo`) VALUES
-	(1, 'Camilo', 'Rodriguez', '1006679966', '3136997711', 'enderkramcr482@gmail.com', 'La esmeralda', 1, 1);
+	(1, 'Miguel', 'Caicedo', '123456789', '1234567999', 'miguelcaiced@gmail.com', 'Mocoa', 1, 1);
 /*!40000 ALTER TABLE `empleados` ENABLE KEYS */;
 
--- Volcando estructura para tabla software_contable.facturas_compras
+-- Volcando estructura para tabla software_contables.facturas_compras
 CREATE TABLE IF NOT EXISTS `facturas_compras` (
   `id_factura_compra` int(11) NOT NULL AUTO_INCREMENT,
-  `fecha` date NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `id_codigo_producto` int(11) NOT NULL DEFAULT '0',
   `cantidad` int(11) NOT NULL DEFAULT '0',
   `val_unitario` int(11) NOT NULL DEFAULT '0',
   `descuento` int(11) NOT NULL DEFAULT '0',
   `id_proveedor` int(11) NOT NULL DEFAULT '0',
-  `id_producto` int(11) NOT NULL DEFAULT '0',
   `impuesto` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_factura_compra`),
-  KEY `FK_id_proveedor` (`id_proveedor`),
-  KEY `FK_id_producto_compra` (`id_producto`),
-  CONSTRAINT `FK_id_producto_compra` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_producto`),
-  CONSTRAINT `FK_id_proveedor` FOREIGN KEY (`id_proveedor`) REFERENCES `proveedores` (`id_proveedor`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Volcando datos para la tabla software_contable.facturas_compras: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `facturas_compras` DISABLE KEYS */;
-/*!40000 ALTER TABLE `facturas_compras` ENABLE KEYS */;
-
--- Volcando estructura para tabla software_contable.facturas_ventas
-CREATE TABLE IF NOT EXISTS `facturas_ventas` (
-  `id_fact_venta` int(11) NOT NULL AUTO_INCREMENT,
-  `fecha` date DEFAULT NULL,
-  `id_cliente` int(11) DEFAULT NULL,
-  `valor_unitario` int(11) DEFAULT NULL,
-  `id_producto` int(11) DEFAULT NULL,
-  `cantidad` int(11) DEFAULT NULL,
-  `valor_total` int(11) DEFAULT NULL,
-  `descuento` int(11) DEFAULT NULL,
-  `id_empleado` int(11) DEFAULT NULL,
-  `id_modo_pago` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_fact_venta`),
-  KEY `FK_id_empleado` (`id_empleado`),
-  KEY `FK_id_modo_pago` (`id_modo_pago`),
-  KEY `FK_id_cliente` (`id_cliente`),
-  KEY `FK_valor_unitario` (`valor_unitario`),
-  KEY `FK_id_producto` (`id_producto`),
-  CONSTRAINT `FK_id_cliente` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id_cliente`),
-  CONSTRAINT `FK_id_empleado` FOREIGN KEY (`id_empleado`) REFERENCES `empleados` (`id_empleado`),
-  CONSTRAINT `FK_id_modo_pago` FOREIGN KEY (`id_modo_pago`) REFERENCES `modos_de_pago` (`id_modo_pago`),
-  CONSTRAINT `FK_id_producto` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_producto`),
-  CONSTRAINT `FK_valor_unitario` FOREIGN KEY (`valor_unitario`) REFERENCES `productos` (`precio_venta`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Volcando datos para la tabla software_contable.facturas_ventas: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `facturas_ventas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `facturas_ventas` ENABLE KEYS */;
-
--- Volcando estructura para tabla software_contable.kardex
-CREATE TABLE IF NOT EXISTS `kardex` (
-  `id_kardex` int(11) NOT NULL AUTO_INCREMENT,
-  `id_factura_venta` int(11) DEFAULT NULL,
-  `id_producto` int(11) DEFAULT NULL,
-  `id_factura_compra` int(11) DEFAULT NULL,
-  `Existencia` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_kardex`),
-  KEY `FK_kardex_id_producto` (`id_producto`),
-  KEY `FK_kardex_id_factura_venta` (`id_factura_venta`),
-  KEY `FK_kardex_id_factura_compra` (`id_factura_compra`),
-  CONSTRAINT `FK_kardex_id_factura_compra` FOREIGN KEY (`id_factura_compra`) REFERENCES `facturas_compras` (`id_factura_compra`),
-  CONSTRAINT `FK_kardex_id_factura_venta` FOREIGN KEY (`id_factura_venta`) REFERENCES `facturas_ventas` (`id_fact_venta`),
-  CONSTRAINT `FK_kardex_id_producto` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_producto`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Volcando datos para la tabla software_contable.kardex: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `kardex` DISABLE KEYS */;
-/*!40000 ALTER TABLE `kardex` ENABLE KEYS */;
-
--- Volcando estructura para tabla software_contable.login
-CREATE TABLE IF NOT EXISTS `login` (
-  `id_login` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL DEFAULT '',
-  `password` varchar(50) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id_login`)
+  KEY `FK_facturas_compras_proveedores` (`id_proveedor`),
+  KEY `FK_facturas_compras_id_productos` (`id_codigo_producto`) USING BTREE,
+  CONSTRAINT `FK_facturas_compras_id_productos` FOREIGN KEY (`id_codigo_producto`) REFERENCES `productos` (`id_producto`),
+  CONSTRAINT `FK_facturas_compras_proveedores` FOREIGN KEY (`id_proveedor`) REFERENCES `proveedores` (`id_proveedores`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla software_contable.login: ~0 rows (aproximadamente)
+-- Volcando datos para la tabla software_contables.facturas_compras: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `facturas_compras` DISABLE KEYS */;
+INSERT INTO `facturas_compras` (`id_factura_compra`, `fecha`, `id_codigo_producto`, `cantidad`, `val_unitario`, `descuento`, `id_proveedor`, `impuesto`) VALUES
+	(1, '2022-05-16 15:24:56', 4, 5, 8, 55, 1, 22);
+/*!40000 ALTER TABLE `facturas_compras` ENABLE KEYS */;
+
+-- Volcando estructura para tabla software_contables.login
+CREATE TABLE IF NOT EXISTS `login` (
+  `id_login` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_login`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- Volcando datos para la tabla software_contables.login: ~1 rows (aproximadamente)
 /*!40000 ALTER TABLE `login` DISABLE KEYS */;
 INSERT INTO `login` (`id_login`, `username`, `password`) VALUES
 	(1, 'admin', 'admin123');
 /*!40000 ALTER TABLE `login` ENABLE KEYS */;
 
--- Volcando estructura para tabla software_contable.modos_de_pago
-CREATE TABLE IF NOT EXISTS `modos_de_pago` (
-  `id_modo_pago` int(11) NOT NULL AUTO_INCREMENT,
-  `modo` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id_modo_pago`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
--- Volcando datos para la tabla software_contable.modos_de_pago: ~2 rows (aproximadamente)
-/*!40000 ALTER TABLE `modos_de_pago` DISABLE KEYS */;
-INSERT INTO `modos_de_pago` (`id_modo_pago`, `modo`) VALUES
-	(1, 'efectivo'),
-	(2, 'tarjeta');
-/*!40000 ALTER TABLE `modos_de_pago` ENABLE KEYS */;
-
--- Volcando estructura para tabla software_contable.productos
+-- Volcando estructura para tabla software_contables.productos
 CREATE TABLE IF NOT EXISTS `productos` (
   `id_producto` int(11) NOT NULL AUTO_INCREMENT,
   `codigo_producto` int(11) NOT NULL DEFAULT '0',
   `nombre` varchar(100) NOT NULL DEFAULT '0',
-  `precio_venta` int(11) DEFAULT '0',
+  `precio_venta` int(11) NOT NULL DEFAULT '0',
   `id_categoria` int(11) NOT NULL DEFAULT '0',
-  `precio_compra` int(11) DEFAULT '0',
+  `existencia` int(11) NOT NULL,
+  `precio_compra` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_producto`),
-  UNIQUE KEY `precio_venta` (`precio_venta`),
-  KEY `FK_id_categoria` (`id_categoria`),
-  KEY `FK_id_precio_compra` (`precio_compra`),
-  CONSTRAINT `FK_id_categoria` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id_categoria`),
-  CONSTRAINT `FK_id_precio_compra` FOREIGN KEY (`precio_compra`) REFERENCES `facturas_compras` (`id_factura_compra`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+  KEY `FK_productos_id_categoría` (`id_categoria`),
+  CONSTRAINT `FK_productos_id_categoría` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id_categoria`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla software_contable.productos: ~0 rows (aproximadamente)
+-- Volcando datos para la tabla software_contables.productos: ~3 rows (aproximadamente)
 /*!40000 ALTER TABLE `productos` DISABLE KEYS */;
+INSERT INTO `productos` (`id_producto`, `codigo_producto`, `nombre`, `precio_venta`, `id_categoria`, `existencia`, `precio_compra`) VALUES
+	(1, 1, 'Coca Cola', 15000, 1, 150, 15000),
+	(2, 2, 'Pepsi', 1, 1, 16, 15),
+	(3, 6, 'Caldo', 12422, 1, 5, -25),
+	(4, 7, 'Caldo', 12422, 1, 5, -25);
 /*!40000 ALTER TABLE `productos` ENABLE KEYS */;
 
--- Volcando estructura para tabla software_contable.proveedores
+-- Volcando estructura para tabla software_contables.proveedores
 CREATE TABLE IF NOT EXISTS `proveedores` (
-  `id_proveedor` int(11) NOT NULL AUTO_INCREMENT,
+  `id_proveedores` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL DEFAULT '0',
   `nit` varchar(50) NOT NULL DEFAULT '0',
   `direccion` varchar(50) NOT NULL DEFAULT '0',
   `celular` varchar(10) NOT NULL DEFAULT '0',
   `correo` varchar(100) NOT NULL DEFAULT '0',
   `ciudad` varchar(50) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id_proveedor`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id_proveedores`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla software_contable.proveedores: ~0 rows (aproximadamente)
+-- Volcando datos para la tabla software_contables.proveedores: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `proveedores` DISABLE KEYS */;
+INSERT INTO `proveedores` (`id_proveedores`, `nombre`, `nit`, `direccion`, `celular`, `correo`, `ciudad`) VALUES
+	(1, 'JugosStiven', '1024556', 'El chorizo', '313696969', 'stiven123@gmail.com', 'Mocoa');
 /*!40000 ALTER TABLE `proveedores` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
